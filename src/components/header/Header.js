708b4b1dc17d84c5dce73ad5115a3077240f01df
Schegol4ik <iliyinchikartem@gmail.com>
2023-12-26 00:16:46 +0100

    v0

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './Header.scss'
 import Navbar from "../navbar/Navbar";
+import logo from '../../assets/images/logo.svg'
+import favorites from '../../assets/images/favorites.svg'
+import basket from '../../assets/images/basket.svg'
 
 const Header = () => {
     return (
@@ -8,20 +11,24 @@ const Header = () => {
             <div className="container">
                 <div className="wrapper_header">
                     <div className="logo_header">
-                        <img src="" alt="logo"/>
-                        <span>Колбасы и мясные деликатесы</span>
+                        <img src={logo} alt="logo"/>
+                        <span>Колбасы и мясные <br/>деликатесы</span>
                     </div>
                     <div className="others_header">
-                        <div>
-                            <img src="" alt="star"/>
-                            <img src="" alt="ellipse"/>
+                        <div className='favorites_star'>
+                            <img src={favorites} alt="star"/>
                         </div>
-                        <div>
-                            <img src="" alt="basket"/>
-                            <p>
-                                <span>Товаров</span>
-                                <h6>0</h6>
-                            </p>
+                        <div className="favorites_header">
+                            <div className='basket_favorites'>
+                                <img src={basket} alt="basket"/>
+                            </div>
+                            <div className='count_favorites'>
+                                <span>Товаров: </span>
+                                <span className='count_goods'>0</span>
+                                <br/>
+                                <span className='count_price'>21 321</span>
+                                <span className='count_currency'>₽</span>
+                            </div>
                         </div>
                     </div>
                 </div>
